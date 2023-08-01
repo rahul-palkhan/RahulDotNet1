b06d6d8166698995564fbc35ff2d0e7cbf9231f1
Rahul Palkhan <rpalkhan@uworld.in>
2023-08-01 17:45:24 +0530

    me

---
+++ service.js
@@ -0,0 +1,10 @@
+document.getElementById("card").addEventListener("click", redirectFunction);
+document.getElementById("card1").addEventListener("click", redirectFunction);
+document.getElementById("card2").addEventListener("click", redirectFunction);
+document.getElementById("card3").addEventListener("click", redirectFunction);
+document.getElementById("card4").addEventListener("click", redirectFunction);
+document.getElementById("card5").addEventListener("click", redirectFunction);
+      
+function redirectFunction(){
+    window.location.href = "service-detail.html";
+    }
